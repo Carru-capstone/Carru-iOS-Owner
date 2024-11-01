555332c8b883f5eb331bed0bf87144b0d9a779f7
Juhun <wngns8334@gmail.com>
2024-11-01 22:06:56 +0900

    프로젝트 생성

---
+++ CarruOwner/CarruOwner/CarruOwnerApp.swift
@@ -0,0 +1,17 @@
+//
+//  CarruOwnerApp.swift
+//  CarruOwner
+//
+//  Created by 이주훈 on 11/1/24.
+//
+
+import SwiftUI
+
+@main
+struct CarruOwnerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
